5872443cd31b21f1aac25c019213c77bf25cd07f
DevilHunterV <vinicud@yahoo.in>
2022-02-21 13:35:19 +0530

    R programming Coursera

---
+++ Missing Values.R
@@ -0,0 +1,15 @@
+##Missing Values
+
+## missing values are denoted as NA or NaN(undefined mathematical operations)
+
+x <- c(1, 2, NA, 10, 3)
+
+is.na(x)
+
+is.nan(x)
+
+y <- c(3,NA , NaN, NA, 9)
+
+is.na(y)
+
+is.nan(y)
